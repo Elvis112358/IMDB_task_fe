d520bd1ab929696fb8394af7bd221ecca6af00ed
Elvis Sehic <elvis.sehic2@gmail.com>
2023-09-01 16:03:57 +0200

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatSidenavContainer } from '@angular/material/sidenav';
+import { UserNavigationItems, PermissionNavigationItems, RouterPageDescriptions } from './core/navigation-items';
+import { NavigationService } from './core/services/navigation.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'imdb_task_fe';
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+  title = 'imdbFront';
+  @ViewChild("sidenavContainer", { static: false })
+  sideNavContainer!: MatSidenavContainer;
+
+  userNavigationItems = UserNavigationItems;
+  permissionNavigationItems = PermissionNavigationItems;
+
+  private pageDescriptions = RouterPageDescriptions;
+  public page: string | undefined;
+
+  constructor(
+    public navigationService: NavigationService
+  ) {
+    //subscribe to router change so we can display "breadcrumb"
+    this.navigationService.currentUrl.subscribe(value => {
+      if(value)
+        this.page = this.pageDescriptions.find(x => value.includes(x.url))?.text;
+    });
+  }
+
+  ngOnInit(): void {
+    //check user screen witth and set sidebar mode
+    if (window.innerWidth > 1280) {
+      this.navigationService.sidenavMode = 'side';
+      this.navigationService.isOpen = true;
+    }
+    else {
+      this.navigationService.sidenavMode = 'over';
+      this.navigationService.isOpen = false;
+    }
+  }
+
+  ngAfterViewInit() {
+    //sometimes sidebar margins are not right so we need to set them manually
+    setTimeout(() => {
+      this.sideNavContainer.updateContentMargins();
+    }, 0);
+  }
+
+  ngOnDestroy(): void {
+    //unsubscribe from navigation service to prevent memory leaks
+    this.navigationService.currentUrl.unsubscribe();
+  }
 }
