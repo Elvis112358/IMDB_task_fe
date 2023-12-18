9794db48fb0258190fac86c6c93caf7eceb4f48f
Elvis Sehic <elvis.sehic2@gmail.com>
2023-12-18 17:26:15 +0100

    implement banner latest

--- src/app/core/components/banners/banner/banner.component.ts
+++ src/app/core/components/banners/banner/banner.component.ts
@@ -1,10 +1,27 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Banner } from 'src/app/core/interfaces/common.interface';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
+  animations: [
+    trigger('simpleFadeAnimation', [
+      transition('*=>*', [
+          style({opacity: 0.7}),
+          animate(800)
+      ])
+  ])
+    // trigger('fadeInOut', [
+    //   // transition('*=>*', [
+    //   //   style({ opacity: 0 }),
+    //   //   animate('0.5s 500ms', style({ opacity: 1 })),
+    //   // ]),
+    //   transition('*=>*', [animate(700, style({ opacity: 0.8 }))]),
+    // ]),
+]
+
 })
 export class BannerComponent {
   @Input() banner!: Banner;
--- src/app/core/components/banners/banners.component.ts
+++ src/app/core/components/banners/banners.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit, inject } from '@angular/core';
 import { BehaviorSubject, EMPTY, Observable, combineLatest, concatMap, debounceTime, interval, map, of, startWith, switchMap, pairwise, take, tap, withLatestFrom } from 'rxjs';
 import { Banner } from '../../interfaces/common.interface';
 import { BannerService } from '../../services/banner.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FadeIn } from '../../animation';
 
 @Component({
   selector: 'app-banners',
   templateUrl: './banners.component.html',
   styleUrls: ['./banners.component.scss'],
+  animations: [FadeIn(200, true)]
 })
-export class BannersComponent implements OnInit {
+export class BannersComponent {
   banners$: Observable<Banner[]> = inject(BannerService).getBanners();
 
   defaultBanner: Banner = {
@@ -27,14 +30,14 @@ export class BannersComponent implements OnInit {
     image:
       'https://img.freepik.com/premium-photo/bangkok-thailand-08082022-lamborghini-luxury-super-car-fast-sports-premium-lighting-background-3d-illustration_67092-1599.jpg',
   };
-  readonly bannerInterval = 4000;
-  // private activeIndex = new BehaviorSubject(0);
-  // activeIndex$ = this.activeIndex.asObservable();
+  readonly bannerInterval = 3500;
+
   currentBannerIndex = 0;
   actionStreamBS: BehaviorSubject<number> = new BehaviorSubject(0);
-  actionStream$: Observable<number> = this.actionStreamBS.asObservable().pipe(startWith(0));
-  interval$:Observable<number> = interval(this.bannerInterval)
-  .pipe(
+  actionStream$: Observable<number> = this.actionStreamBS
+    .asObservable()
+    .pipe(startWith(0));
+  interval$: Observable<number> = interval(this.bannerInterval).pipe(
     startWith(0),
     switchMap(() =>
       this.actionStream$.pipe(
@@ -43,70 +46,34 @@ export class BannersComponent implements OnInit {
       )
     )
   );
-  // banner$ = combineLatest([this.banners$, this.actionStream$]).pipe(
-  //   tap(()=>console.log('this.CurrentIndex', this.currentBannerIndex)),
-  //   map(([banners, action]: [Banner[], number]) => {
-  //     return this.getBannerForSlider(banners, action)
-  //   })
-  // );
-  banner$ = combineLatest([this.banners$, this.actionStream$.pipe(pairwise()), this.interval$.pipe(pairwise())])
-    .pipe(
-      map(([banners, [prevAction, action], [prevInterval, currInterval]]: [Banner[], [number, number], [number, number]]) => {
-        
-        console.log('prevAction', prevAction);
-        console.log('action', action);
-        console.log('prevInterval', prevInterval);
-        console.log('currInterval', currInterval);
-        console.log('currentBannerIndex', this.currentBannerIndex);
-        console.log(')_________ ');
 
-        if(action > currInterval && action > prevAction) {
-        
-          console.log('KLIKNO DESNO');
-          return this.getBannerForSlider(banners, action)
-        }
-
-        // if() {
-          if((action < this.currentBannerIndex) && (action === prevInterval || action < currInterval && action < prevAction)) {
-          console.log('KLIKNO LIJEVO');
-          return this.getBannerForSlider(banners, action)
-        }
-        return this.getBannerForSlider(banners, currInterval)
-      })
+  trigger: 'action' | 'interval' = 'interval';
+  banner$ = combineLatest([
+    this.banners$,
+    this.actionStream$.pipe(tap(() => (this.trigger = 'action'))),
+    this.interval$.pipe(tap(() => (this.trigger = 'interval'))),
+  ]).pipe(
+    map(([banners, action, interval]: [Banner[], number, number]) =>
+      this.getBannerForSlider(
+        banners,
+        this.trigger === 'action' ? action : interval
+      )
     )
-
-  ngOnInit(): void {
-    // this.bannerSlider();
-  }
-
-  // nextBannerIndexInterval(): Observable<number | null> {
-  //   return interval(this.bannerInterval)
-  //     .pipe(
-  //       startWith(0),
-  //       switchMap(() =>
-  //         this.actionStream$.pipe(
-  //           debounceTime(this.bannerInterval - 100),
-  //           map(() => this.currentBannerIndex + 1)
-  //         )
-  //       )
-  //     )
-  // }
+  );
 
   nextBannerChange() {
-    this.actionStreamBS.next(this.currentBannerIndex - 1) //TODO promjeniti ime event emitera
+    this.actionStreamBS.next(this.currentBannerIndex - 1); //TODO promjeniti ime event emitera
   }
 
   previousBannerChange() {
-    this.actionStreamBS.next(this.currentBannerIndex + 1)
+    this.actionStreamBS.next(this.currentBannerIndex + 1);
   }
 
-  private getBannerForSlider(banners : Banner[], index: number): Banner {
+  private getBannerForSlider(banners: Banner[], index: number): Banner {
     const calculatedIndex = index % banners.length;
     let indexToReturn =
-      calculatedIndex < 0
-        ? calculatedIndex + banners.length
-        : calculatedIndex;
-    this.currentBannerIndex  = index;
+      calculatedIndex < 0 ? calculatedIndex + banners.length : calculatedIndex;
+    this.currentBannerIndex = index;
     return banners[indexToReturn];
   }
 }
